b18830cbf954db64a3e9efdf8402f19525ca4087
JorLo <jory.laurin@gmail.com>
2022-05-11 18:13:17 -0700

    added transpose function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+/*const wordSearch = (letters, word) => { 
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};*/
+const transpose = function(matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let subArr = [];
+    for (let j = 0; j < matrix.length; j++) {
+      subArr.push(matrix[j][i]);
     }
-}
+    newArr.push(subArr);
+  }
+  return newArr;
+};
+const wordSearch = (letters, word) => { 
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticleJoin = transpose(letters).map(ls => ls.join(''));
+  for (l of verticleJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+/*const backwardJoin = letters.map(ls => ls.join(''));
+for (let i = letters.length -1; i <= 0 ; i--) {
+  module.exports = wordSearch;
+  const backwardJoin = letters.map(ls => ls.reverse().join(''));
+  for (let i = ) {
+      if (p.includes(word)) return true;
+    }
+    return false;*/
